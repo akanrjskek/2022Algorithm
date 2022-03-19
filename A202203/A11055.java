c868a228df14036ae6ad38f839b92ea1801f420f
Yeonghun Won <akanrjskek@naver.com>
2022-03-19 11:34:59 +0900

    아직 못푼 문제

--- A202203/A11055.java
+++ A202203/A11055.java
@@ -1,2 +1,34 @@
-package A202203;public class A11055 {
+package A202203;
+import java.io.*;
+import java.util.*;
+
+public class A11055 {
+    private static int N;
+    private static int[] nums,sums;
+    private static boolean flag = false;
+    public static void main(String[] args) throws Exception{
+        N = Integer.parseInt(bf.readLine());
+        nums = new int[N + 1];
+        sums = new int[N + 1];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        sums[1] = nums[1];
+
+        for(int i = 2; i <= N; ++i){
+            sums[i] = nums[i];
+            for(int j = 1; j < i; ++j){
+                if(nums[i] > nums[j]){
+                    sums[i] = Math.max(nums[i] + sums[j], sums[i]);
+                }
+            }
+        }
+
+        Arrays.sort(sums);
+        System.out.println(sums[N]);
+
+    }
+    private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
 }
--- A202203/A1946.java
+++ A202203/A1946.java
@@ -1,2 +1,54 @@
-package A202203;public class A1946 {
+package A202203;
+
+import java.io.*;
+import java.util.*;
+
+public class A1946 {
+    static class Person implements Comparable<Person>{
+        int first;
+        int second;
+
+        Person(int first, int second){
+            this.first = first;
+            this.second = second;
+        }
+
+        public int compareTo(Person p){
+            return this.first - p.first;
+        }
+    }
+
+    private static int T;
+    private static int N;
+    private static Person[] people;
+    private static int cnt;
+    private static int min;
+
+    public static void main(String[] args) throws Exception{
+        T = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < T; ++i){
+            N = Integer.parseInt(bf.readLine());
+            people = new Person[N];
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(bf.readLine());
+                int f = Integer.parseInt(st.nextToken());
+                int s = Integer.parseInt(st.nextToken());
+                people[j] = new Person(f,s);
+            }
+            Arrays.sort(people);
+            cnt = 1;
+            min = people[0].second;
+            for (int j = 1; j < N; j++) {
+                if(min > people[j].second){
+                    cnt++;
+                    min = people[j].second;
+                }
+            }
+            sb.append(cnt + "\n");
+        }
+        System.out.println(sb);
+    }
+    private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 }
